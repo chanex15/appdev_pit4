17952bd8b6b710de550be08e3607bc1b46f639bf
baldoklienedwind <baldoklienedwind@gmail.com>
2025-04-15 21:18:58 +0400

    Update #1

---
+++ TodoApp/backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from . import models, schemas, crud, database
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/todos", response_model=list[schemas.TodoRead])
+def read_todos(db: Session = Depends(get_db)):
+    return crud.get_todos(db)
+
+@app.get("/todos/{todo_id}", response_model=schemas.TodoRead)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id)
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@app.post("/todos", response_model=schemas.TodoRead)
+def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db, todo)
+
+@app.put("/todos/{todo_id}", response_model=schemas.TodoRead)
+def update(todo_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.update_todo(db, todo_id, todo)
+
+@app.delete("/todos/{todo_id}")
+def delete(todo_id: int, db: Session = Depends(get_db)):
+    crud.delete_todo(db, todo_id)
+    return {"message": "Deleted"}
