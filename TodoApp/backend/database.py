3921fbf5b96be1b2c693ee53bce9907704f53c54
baldoklienedwind <baldoklienedwind@gmail.com>
2025-04-16 13:20:19 +0400

    ehhhhhhh

--- TodoApp/backend/database.py
+++ TodoApp/backend/database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SQLALCHEMY_DATABASE_URL = "postgresql://pit4db_user:wTz0uYm22dOAOx512qiEbpg0ZeLJPbFG@dpg-cvvgd03uibrs73beebh0-a.virginia-postgres.render.com/pit4db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
